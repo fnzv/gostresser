9b0f83a30d107ddb869488f5d801b99e0f82f786
fnzv <samiii@protonmail.com>
2017-10-29 17:37:10 +0000

    testing vegeta integration

--- gostresser.go
+++ gostresser.go
@@ -1,18 +1,27 @@
 package main
 
 
-import "math/rand"
-import "strconv"
-import "os"
-import "time"
-import "net"
-import "fmt"
+import  (
+ "math/rand"
+ "strconv"
+ "os"
+ "time"
+ "net"
+ "fmt"
+ vegeta "github.com/tsenart/vegeta/lib"
+)
+var URL string
 //Author: Sami Yessou
 // Gostresser - Easy and Fast TCP/UDP connection stresser using Goroutines workers
 // it's my first golang project so don't expect any pro dev code, just for fun and testing
 
 func init() {
     rand.Seed(time.Now().UnixNano())
+}
+
+func FloatToString(input_num float64) string {
+    // to convert a float number to a string
+    return strconv.FormatFloat(input_num, 'f', 6, 64)
 }
 
 var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
@@ -34,19 +43,24 @@ func main() {
  if len(os.Args) > 1 {
         hostname := os.Args[1]
         port := os.Args[2]
+	if port == "80" {
+		URL = "http://"+hostname+"/"
+	}
         proto := os.Args[3]
         seconds_args  := os.Args[4]
         num_workers_args := os.Args[5]
-
+//        URL := "http://testcdn2.sami.pw/img.jpg"
         socket := hostname+":"+port
         i63, _ := strconv.ParseInt(seconds_args, 10, 32)
         seconds := int(i63)
         i64, _ := strconv.ParseInt(num_workers_args, 10, 32)
         num_workers := int(i64)
+	fmt.Println("URL IS ", URL)
         fmt.Println("Start Load stressing: ", socket)
         for i := 0; i < num_workers; i++ {
                 go loadtest(socket,proto)
-                fmt.Println("Worker ",i)
+        //        fmt.Println("Worker ",i)
+		vegeta_integration(URL)
                                  }
         time.Sleep(time.Second * time.Duration(seconds))
         } else {
@@ -67,5 +81,28 @@ func loadtest(socket string,proto string){
     //fmt.Println(".")
     // Add multiple requests and non specific to HTTP
         }
+
+}
+
+func vegeta_integration(url string) {
+  rate := uint64(100) // per second
+  duration := 5 * time.Second
+  targeter := vegeta.NewStaticTargeter(vegeta.Target{
+    Method: "GET",
+    URL:    url,
+  })
+  attacker := vegeta.NewAttacker()
+
+  var metrics vegeta.Metrics
+  for res := range attacker.Attack(targeter, rate, duration) {
+    metrics.Add(res)
+  }
+  metrics.Close()
+
+//  fmt.Printf("99th percentile: %s\n", metrics.Latencies.P99)
+  fmt.Printf("\nMax Lantency: %s\n", metrics.Latencies.Total)
+  fmt.Printf("\nMean Lantency: %s\n", metrics.Latencies.Mean)
+  fmt.Printf("\nMean Byts In : %s\n", FloatToString(metrics.BytesIn.Mean))
+  fmt.Printf("\nMean Bytes Out : %s\n", FloatToString(metrics.BytesOut.Mean))
 
 }
